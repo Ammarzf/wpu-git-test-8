caa0385856118ee86db475030e95ec33ffaab87a
Ammar Zaky <amarzakf@gmail.com>
2020-10-07 20:45:32 +0700

    menambahkan lsit

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, expedita alias reprehenderit quisquam praesentium minima mollitia velit veniam sed id sint beatae, corporis rerum explicabo illo enim molestias ea recusandae!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
